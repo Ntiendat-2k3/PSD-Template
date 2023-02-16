b98fcbf479f4b5a185f720ab5e8257851bdabe95
Ntiendat-2k3 <107376328+Ntiendat-2k3@users.noreply.github.com>
2023-02-17 00:45:37 +0700

    source-code

---
+++ gulp-template-master/.history/app/scripts/main_20230216160352.js
@@ -0,0 +1,18 @@
+// window.addEventListener("load", function () {
+//   const toggle = document.querySelector(".menu-toggle");
+//   const menu = document.querySelector(".menu");
+//   toggle && toggle.addEventListener("click", handleToggleMenu);
+//   function handleToggleMenu(e) {
+//     menu && menu.classList.add("is-show");
+//   }
+//   document.addEventListener("click", handleClickOutside);
+//   function handleClickOutside(e) {
+//     if (e.target.matches(".menu-toggle") || e.target.matches(".menu, .menu *"))
+//       return;
+//     menu && menu.classList.remove("is-show");
+//   }
+// });
+
+const menuToggle = document.querySelector(".menu-toggle");
+const menuHeader = document.querySelector(".header-menu");
+const expandClass = 
