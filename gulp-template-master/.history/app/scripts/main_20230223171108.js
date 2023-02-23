f426970bdec5bf8b7190dacb612e3c878504de5e
Ntiendat-2k3 <107376328+Ntiendat-2k3@users.noreply.github.com>
2023-02-23 21:07:22 +0700

    source-code

---
+++ gulp-template-master/.history/app/scripts/main_20230223171108.js
@@ -0,0 +1,68 @@
+// window.addEventListener("load", function () {
+//   const toggle = document.querySelector(".menu-toggle");
+//   const menu = document.querySelector(".menu");
+//   toggle && toggle.addEventListener("click", handleToggleMenu);
+//   function handleToggleMenu(e) {
+//     menu && menu.classList.add("is-show");
+//   }
+//   document.addEventListener("click", handleClickOutside);
+//   function handleClickOutside(e) {
+//     if (e.target.matches(".menu-toggle") || e.target.matches(".menu, .menu *"))
+//       return;
+//     menu && menu.classList.remove("is-show");
+//   }
+// });
+
+const menuToggle = document.querySelector(".header-toggle");
+const menuHeader = document.querySelector(".header-menu");
+const expandClass = "is-expand";
+menuToggle.addEventListener("click", () => {
+     menuHeader.classList.add(expandClass);
+});
+window.addEventListener("click", function (e) {
+     if (!menuHeader.contains(e.target) && !e.target.matches(".header-toggle")) {
+     menuHeader.classList.remove(expandClass);
+     }
+});
+
+// Slick Slider 
+$(document).ready(function(){
+     $(".quote-container").slick({
+          prevArrow:
+               "<button type='button' class='slick-prev pull-left'><i class='fal fa-angle-left' aria-hidden='true'></i></button>",
+          nextArrow:
+               "<button type='button' class='slick-next pull-right'><i class='fal fa-angle-right' aria-hidden='true'></i></button>",
+     });
+     $(".responsive").slick({
+       dots: true,
+       infinite: false,
+       speed: 300,
+       slidesToShow: 4,
+       slidesToScroll: 4,
+       responsive: [
+         {
+           breakpoint: 1024,
+           settings: {
+             slidesToShow: 3,
+             slidesToScroll: 3,
+             infinite: true,
+             dots: true,
+           },
+         },
+         {
+           breakpoint: 600,
+           settings: {
+             slidesToShow: 2,
+             slidesToScroll: 2,
+           },
+         },
+         {
+           breakpoint: 480,
+           settings: {
+             slidesToShow: 1,
+             slidesToScroll: 1,
+           },
+         },
+       ],
+     });
+});
